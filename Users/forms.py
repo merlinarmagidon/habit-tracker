6eefd0f06b22d66ac74bafc6448bed57f51fec42
merlin <merlin.armagidon@gmail.com>
2026-03-30 15:03:42 +0300

    Добавлены медиа-запросы для фона под разрешение 1920x1080

--- Users/forms.py
+++ Users/forms.py
@@ -1,25 +1,35 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+from django.core.exceptions import ValidationError
+import re
 
 
 class UserRegisterForm(UserCreationForm):
-    """
-    Form for registering a new user.
-
-    Attributes:
-    ----------
-        email (EmailField): The email address of the user.
-    """
-
     email = forms.EmailField()
 
     class Meta:
-        """
-        Meta class for defining the model and fields for the UserRegisterForm.
-        """
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
-    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if len(username) > 30:
+            raise ValidationError('Имя пользователя не может превышать 30 символов.')
+        return username
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if not re.match(r'^[а-яА-ЯёЁa-zA-Z]+$', first_name):
+            raise ValidationError('Имя может содержать только буквы.')
+        if len(first_name) > 30:
+            raise ValidationError('Имя не может превышать 30 символов.')
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        if not re.match(r'^[а-яА-ЯёЁa-zA-Z]+$', last_name):
+            raise ValidationError('Фамилия может содержать только буквы.')
+        if len(last_name) > 30:
+            raise ValidationError('Фамилия не может превышать 30 символов.')
+        return last_name
